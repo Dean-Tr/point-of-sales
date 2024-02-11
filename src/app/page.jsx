b7e44f58aef70c730375efb32b5a395db060c717
Dean-Tr <deantirtas@gmail.com>
2024-02-11 23:26:31 +0700

    melanjutkan bagian product list

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import { Products } from "@/data.js";
 
 export default function Home() {
   return (
     <div className="bg-white h-[calc(100vh-3rem)] w-screen flex flex-col md:flex-row">
       {/* LEFT PART */}
-      <div className="h-[60%] md:h-full md:w-[60%]">
+      <div className="h-full w-full flex-[1.5]">
         {/* SEARCH BAR */}
-        <div class="relative mt-2 mx-16  rounded-md shadow-sm">
+        <div class="relative my-2 mx-3 rounded-md shadow-sm">
           <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <Image src="/search.png" alt="" height={20} width={20} />
           </div>
@@ -18,105 +19,40 @@ export default function Home() {
         </div>
 
         {/* PRODUCT LIST */}
-        <div className="w-full h-[calc(100vh-6.5rem)] flex flex-wrap p-3 gap-6 place-content-start justify-center overflow-y-scroll">
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
+        <div className="w-full h-[calc(100vh-24rem)] md:h-[calc(100vh-7rem)] p-3 grid gap-3 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 grid-rows-[13rem_minmax(13rem,13rem)_13rem] justify-items-center items-center overflow-y-scroll">
+          {Products.map((item) => (
+            <div
+              class="w-full h-full max-w-[13rem] max-h-[13rem] flex flex-col justify-between text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer"
+              key={item.id}
+            >
+              <div className="p-2 flex h-full items-center justify-center">
+                <Image src={item.img} alt="product image" width={120} height={120} />
+              </div>
+              <div class="px-3 pb-1">
+                <h5 class="text-sm font-semibold tracking-tight">{item.title}</h5>
+                <span class="text-sm font-bold text-slate-400">
+                  {item.sellPrice.toLocaleString("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                    minimumFractionDigits: 0,
+                  })}
+                </span>
               </div>
             </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-64 h-fit max-h-64 text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer">
-            <div className="p-4 flex items-center justify-center">
-              <Image src="/temporary/p1.png" alt="product image" width={150} height={150} />
-            </div>
-            <div class="px-5 pb-5">
-              <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight">Beef and Cheese</h5>
-              </a>
-              <div class="flex  items-center justify-between">
-                <span class="text-xl font-bold text-slate-400">Rp20.000</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
 
       {/* RIGHT PART */}
-      <div className="h-[40%] md:h-full md:w-[40%] border-t-2 border-t-slate-600 md:border-l-2 md:border-l-slate-400">
+      <div className="h-full w-full flex-1 border-t-2 border-t-slate-600 md:border-l-2 md:border-l-slate-400 text-black bg-white">
         <div className="bg-[#40A2E3] h-12 w-full flex flex-row-reverse items-center pr-6 cursor-pointer">
           <Image src="/delete.png" alt="" width={25} height={25} />
+        </div>
+        <div className="">
+          <h1>asdasd</h1>
+          <h1>asdasd</h1>
+          <h1>asdasd</h1>
+          <h1>asdasd</h1>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 bg-blue-600 flex items-center justify-between uppercase px-12">
+    <div className="h-12 bg-blue-600 flex items-center justify-between uppercase px-12 text-white">
       <div className="hidden md:flex flex-1 text-xl font-bold">
         <Link href="/">Warung Bu Imas</Link>
       </div>
