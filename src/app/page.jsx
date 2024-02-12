e98b9620451649b1e8e151d9eed0c7bd417c00ff
Dean-Tr <deantirtas@gmail.com>
2024-02-12 23:44:45 +0700

    menambah detail pembelian

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,16 +19,16 @@ export default function Home() {
         </div>
 
         {/* PRODUCT LIST */}
-        <div className="w-full h-[calc(100vh-24rem)] md:h-[calc(100vh-7rem)] p-3 grid gap-3 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 grid-rows-[13rem_minmax(13rem,13rem)_13rem] justify-items-center items-center overflow-y-scroll">
+        <div className="w-full h-[calc(100vh-24rem)] md:h-[calc(100vh-7rem)] p-3 grid gap-3 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 grid-rows-[13rem] justify-items-center items-center overflow-y-scroll">
           {Products.map((item) => (
             <div
-              class="w-full h-full max-w-[13rem] max-h-[13rem] flex flex-col justify-between text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer"
+              class="w-full max-w-[13rem] h-[13rem] min-w-[8rem] flex flex-col justify-between text-slate-700 bg-white border border-gray-200 rounded-lg shadow cursor-pointer"
               key={item.id}
             >
-              <div className="p-2 flex h-full items-center justify-center">
-                <Image src={item.img} alt="product image" width={120} height={120} />
+              <div className="relative w-full h-full flex items-center justify-center">
+                <Image src={item.img} alt="product image" fill objectFit="contain" />
               </div>
-              <div class="px-3 pb-1">
+              <div class="px-3 py-2">
                 <h5 class="text-sm font-semibold tracking-tight">{item.title}</h5>
                 <span class="text-sm font-bold text-slate-400">
                   {item.sellPrice.toLocaleString("id-ID", {
@@ -44,15 +44,56 @@ export default function Home() {
       </div>
 
       {/* RIGHT PART */}
-      <div className="h-full w-full flex-1 border-t-2 border-t-slate-600 md:border-l-2 md:border-l-slate-400 text-black bg-white">
-        <div className="bg-[#40A2E3] h-12 w-full flex flex-row-reverse items-center pr-6 cursor-pointer">
-          <Image src="/delete.png" alt="" width={25} height={25} />
+      <div className="h-full w-full flex-1 flex flex-col items-center justify-between border-t-2 border-t-slate-600 md:border-l-2 md:border-l-slate-400 text-black bg-white">
+        {/* DELETE BUTTON */}
+        <div className="bg-[#40A2E3] w-full px-6 py-3 flex justify-between items-center">
+          <p className="text-base font-bold text-white">Detail Pembelian</p>
+          <button className="cursor-pointer">
+            <Image src="/delete.png" alt="" width={25} height={25} />
+          </button>
         </div>
-        <div className="">
-          <h1>asdasd</h1>
-          <h1>asdasd</h1>
-          <h1>asdasd</h1>
-          <h1>asdasd</h1>
+
+        {/* BUY PRODUCT */}
+        <div className="w-full h-full max-h-[10.3rem] md:max-h-full max py-3 flex flex-col justify-start items-center gap-4 overflow-y-scroll">
+          {Products.map((item) => (
+            <div className="w-full px-6 flex gap-2   justify-between items-center" key={item.id}>
+              <div className="w-[25%]">
+                <Image src="/temporary/p18.png" alt="product image" width={60} height={60} />
+              </div>
+              <div className="w-[25%]">
+                <p className="text-sm lg:text-xl font-bold">Sicilian </p>
+                <span className="text-xs md:text-[0.5rem] lg:text-xs font-bold text-slate-400">
+                  Rp 30.000
+                </span>
+              </div>
+              <div className="w-[40%] flex justify-evenly items-center">
+                <button className="w-8 h-8 md:w-6 md:h-6 lg:w-8 lg:h-8 flex justify-center items-center bg-blue-300 rounded-full text-xl md:text-base lg:text-xl text-white">
+                  {"<"}
+                </button>
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  className="w-14 md:w-9 text-center outline-none"
+                />
+                <button className="w-8 h-8 md:w-6 md:h-6 lg:w-8 lg:h-8 flex justify-center items-center bg-blue-300 rounded-full text-xl md:text-base lg:text-xl text-white">
+                  {">"}
+                </button>
+              </div>
+              <button className="w-[10%] flex justify-end items-center cursor-pointer">
+                <Image src="/close.png" alt="" width={20} height={20} />
+              </button>
+            </div>
+          ))}
+        </div>
+
+        {/* TOTAL PRICE */}
+        <div className="w-full flex justify-between">
+          <p className="flex justify-center items-center w-full h-full py-4 bg-[#40A2E3] font-bold text-white">
+            Rp 150.000
+          </p>
+          <button className="w-full h-full bg-[#0D9276] text-white text-xl font-bold ">
+            Bayar
+          </button>
         </div>
       </div>
     </div>
