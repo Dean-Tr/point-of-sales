2a15c24ea7fff58143006cb62845cebc7d708812
Dean-Tr <deantirtas@gmail.com>
2024-06-11 20:14:58 +0700

    rubah error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,16 +1,29 @@
-"use client";
+// pages/_error.js
+import { useEffect } from "react";
 
-const Error = ({ statusCode }) => {
+const Error = ({ statusCode, message, stack }) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error({ statusCode, message, stack });
+  }, [statusCode, message, stack]);
+
   return (
-    <p>
-      {statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}
-    </p>
+    <div className="flex justify-center items-center">
+      <h1>
+        {statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}
+      </h1>
+      {message && <p>{message}</p>}
+      {stack && <pre>{stack}</pre>}
+    </div>
   );
 };
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+  const message = err ? err.message : "Unknown error";
+  const stack = err ? err.stack : null;
+
+  return { statusCode, message, stack };
 };
 
 export default Error;
